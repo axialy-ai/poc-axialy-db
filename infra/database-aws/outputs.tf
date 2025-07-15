32c2a8ad73d1e86f71f8a4c85301c3580941d679
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 15:02:14 -0700

    Update outputs.tf

--- infra/database-aws/outputs.tf
+++ infra/database-aws/outputs.tf
@@ -9,22 +9,22 @@ output "db_port" {
 }
 
 output "db_admin_user" {
-  description = "Database admin username"
+  description = "Admin username"
   value       = aws_db_instance.axialy.username
 }
 
 output "db_admin_password" {
-  description = "Database admin password"
+  description = "Admin password"
   value       = random_password.db_password.result
   sensitive   = true
 }
 
 output "db_instance_id" {
-  description = "The RDS instance ID"
+  description = "RDS instance ID"
   value       = aws_db_instance.axialy.id
 }
 
 output "security_group_id" {
-  description = "ID of the RDS security group"
+  description = "RDS security group ID"
   value       = aws_security_group.rds.id
 }
