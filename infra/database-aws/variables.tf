a2b5a6298ccb834b0e3d9272fbcb166c32bf3ddd
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 14:18:37 -0700

    Update variables.tf

--- infra/database-aws/variables.tf
+++ infra/database-aws/variables.tf
@@ -1,1 +1,54 @@
 
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-west-1"
+}
+
+variable "db_identifier" {
+  description = "Identifier for the RDS instance"
+  type        = string
+  default     = "axialy-database-cluster"
+}
+
+variable "instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  description = "Allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "multi_az" {
+  description = "Enable Multi-AZ deployment for high availability"
+  type        = bool
+  default     = false
+}
+
+variable "backup_retention_period" {
+  description = "Number of days to retain backups"
+  type        = number
+  default     = 7
+}
+
+variable "backup_window" {
+  description = "Preferred backup window"
+  type        = string
+  default     = "07:00-09:00"
+}
+
+variable "maintenance_window" {
+  description = "Preferred maintenance window"
+  type        = string
+  default     = "sun:09:00-sun:11:00"
+}
+
+variable "admin_default_user" {
+  description = "Default admin username"
+  type        = string
+  default     = "axialy_admin"
+}
